fcf6cc07608794b47c5ebf5de5a8c00ecbd14cbc
jordan-grindrod <JLGrindrod@hotmail.com>
2019-05-23 15:32:35 +0100

    added functionality to show tasks

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Dashboard msg="Welcome to Chaperoo To-Do"/>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Dashboard from './components/Dashboard.vue'
+import axios from "axios";
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Dashboard
+  },
 }
+
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0d7be9;
   margin-top: 60px;
 }
+
 </style>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,38 +1,55 @@
 <template>
-  <div class="hello">
+  <div class="dashboard">
+
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    <p class="subhead">
+      The Perfect Task Management Solution
     </p>
- WOOOOOOO CHAPEROO
+    <p class="table" v-for="task in tasks" v-bind:key="task.task_id">
+      {{ task.task_id }}
+      {{ task.task }}
+      {{ task.status }}
+    </p>
+
   </div>
 </template>
 
 <script>
+
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: 'Dashboard',
   props: {
-    msg: String
+    msg: String,
+  },
+  data(){
+    return {
+      tasks: []
+    }
+  },
+  created:function(){
+    this.getTasks();
+  },
+  methods: {
+    getTasks: function(){
+      axios.get("http://localhost:3000/task/gettasks").then((response)=>{
+        this.tasks = response.data;       
+      }).catch()
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
+
+h1 {
   margin: 40px 0 0;
+      }
+
+.table{
+  color:black
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
